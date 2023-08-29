246cc5e2bb6888e603fb1357b85f64094322d3a2
almasi2 <franklinekimeria@gmail.com>
2023-08-29 09:33:13 -0700

    variables

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+/*
+ * File: 4-print_alphabt.c
+ * Auth: Frankline Kimeria
+ */
+#include <stdio.h>
+
+/**
+ * main -   prints the alphabet in lowercase
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a' ; letter <= 'z' ; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
