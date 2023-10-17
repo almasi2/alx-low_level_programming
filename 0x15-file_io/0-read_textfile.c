d862fe7bd83fea213f2c0be82f79526d75f79620
almasi2 <franklinekimeria@gmail.com>
2023-10-16 22:07:12 -0700

    main.h

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+/**
+ * read_textfile - reads file text and prints the letters
+ * @filename: name of file
+ * @letters: number of letters that are printed.
+ * Return: numbers of letters print, returns 0 if it fails.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,20 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/*
+ * File: holberton.h
+ * Auth: Frankline Kimeria (almasi2)
+ * Desc: Prototypes for all functions are contained here.
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
